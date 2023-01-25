8508a0dc3fa58ee64778766caa7fca0191569c23
theoistic <theo@theoistic.com>
2023-01-25 17:09:01 +0000

    dotnet 7 changes

---
+++ TCMD.TestCLI/Program.cs
@@ -0,0 +1,24 @@
+﻿
+using TCMD;
+
+Command.Add("test", (int a, bool g3, string t) =>
+{
+    Console.WriteLine($"This is the test.. \n 'a' having the value of {a}\n 'g3' having the value of {g3}\n 't' having the value of {t}..");
+});
+
+await Command.ParseArguments();
+
+public static class YAY
+{
+    [CMD]
+    public static void Add(int a, int b = 2)
+    {
+        Console.WriteLine($"result: {a + b}");
+    }
+
+    [CMD]
+    public static void Div(double a, double b)
+    {
+        Console.WriteLine($"result: {a / b}");
+    }
+}
--- TCMD/TCMD.cs
+++ TCMD/TCMD.cs
@@ -1,91 +1,277 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Net;
 using System.Reflection;
+using System.Reflection.Metadata;
 using System.Text;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
-namespace TCMD
+namespace TCMD;
+
+internal class TDynamicMethod
 {
-    public static class TCMD
+    public string Name { get; private set; }
+    public Delegate Action { get; private set; }
+    public TDMParameter[]? Parameters { get; private set; }
+
+    public TDynamicMethod(string name, Delegate action)
     {
-        public static async Task Parse(params string[] args)
+        Name = name;
+        Action = action;
+        Parameters = Action.Method.GetParameters().Select(x => new TDMParameter(x.Name ?? x.Position.ToString(), x.ParameterType, x.DefaultValue)).ToArray();
+    }
+
+    public Task? Invoke(TArgument[]? args)
+    {
+        object?[]? parameters = new object?[Parameters?.Length ?? 0];
+        for (int i = 0; i < Parameters?.Length; i++)
         {
-            if (args.Length == 0)
-                throw new ArgumentException("No parameters given.");
-
-            var assembly = Assembly.GetEntryAssembly();
-            var methods = assembly.GetTypes().SelectMany(t => t.GetMethods()).Where(m => m.GetCustomAttributes(typeof(CMD), false).Length > 0).ToArray();
-            MethodInfo func = methods.FirstOrDefault(x => x.Name.ToLower() == args[0].ToLower());
-            if (func == null) throw new ArgumentException($"Command {args[0]} not found.");
-            List<object> newArgs = null;
-            if (func.GetParameters().Any())
+            var parameter = Parameters[i];
+            var arg = args?.FirstOrDefault(x => x.Name == parameter.Name);
+            if (arg == null)
             {
-                newArgs = new List<object>();
-                int requiredParameters = func.GetParameters().Where(x => x.HasDefaultValue == false).Count();
-                IEnumerable<string> GetArgument(string option) => args.SkipWhile(i => i.Substring(1, i.Length - 1).ToLower() != option.ToLower()).Skip(1).SelectArgChunks();
-                foreach (var p in func.GetParameters())
+                if (parameter.DefaultValue == null)
                 {
-                    var paramValue = GetArgument(p.Name);
-                    if (paramValue == null && p.DefaultValue != null)
-                    {
-                        throw new ArgumentException($"Parameter '{p.Name}' is required.");
-                    }
-                    else if (p.ParameterType.IsArray)
-                    {
-
-                        newArgs.Add(paramValue.ToArray());
-                    }
-                    else
-                    {
-                        var val = paramValue.FirstOrDefault();
-                        if (int.TryParse(val, out var intVal))
-                        {
-                            newArgs.Add(intVal);
-                        } else if(bool.TryParse(val, out var boolVal))
-                        {
-                            newArgs.Add(boolVal);
-                        }
-                        else
-                        {
-                            newArgs.Add(val);
-                        }
-                    }
+                    Command.Error($"Missing argument {parameter.Name}");
+                    return Task.CompletedTask;
+                }
+                try
+                {
+                    parameters[i] = Convert.ChangeType(parameter.DefaultValue, parameter.Type);
+                } catch
+                {
+                    Command.Error($"Failed to convert argument -{parameter.Name} with value '{parameter.DefaultValue}' to {parameter.Type.Name}");
+                    return Task.CompletedTask;
                 }
             }
-            if (func.ReturnType == typeof(Task))
+            else if(arg.Value != null)
             {
-                await (Task)func.Invoke(null, newArgs == null ? null : newArgs.ToArray());
-            }
-            else
-            {
-                await Task.Run(() => func.Invoke(null, newArgs == null ? null : newArgs.ToArray()));
+                try
+                {
+                    parameters[i] = Convert.ChangeType(arg.Value, parameter.Type);
+                }
+                catch 
+                {
+                    Command.Error($"Failed to convert argument -{parameter.Name} with value '{arg.Value}' to {parameter.Type.Name}");
+                    return Task.CompletedTask;
+                }
             }
         }
+        if (Action.Method.ReturnType == typeof(Task))
+            return (Task?)Action.DynamicInvoke(parameters);
+        else return Task.Run(() => Action.DynamicInvoke(parameters));
+    }
 
-        public static IEnumerable<string> SelectArgChunks(this IEnumerable<string> source)
+    public class TDMParameter
+    {
+        public string Name { get; private set; }
+        public Type Type { get; private set; }
+        public bool HasDefaultValue { get; private set; }
+        public object? DefaultValue { get; private set; }
+
+        public TDMParameter(string name, Type type, object? defaultValue = null)
         {
-            foreach (var item in source)
-            {
-                if (item.StartsWith("-"))
-                {
-                    break;
-                }
-                else
-                {
-                    yield return item;
-                }
-            }
+            Name = name;
+            Type = type;
+            HasDefaultValue = defaultValue != null;
+            DefaultValue = defaultValue;
+        }
+    }
+}
+
+public class TArgument
+{
+    public string Name { get; private set; }
+    public object? Value { get; private set; }
+
+    public TArgument(string name, object? value = null)
+    {
+        Name = name;
+        Value = value;
+    }
+}
+
+public static class Command
+{
+    public static bool DefaultToHelp { get; set; } = true;
+    private static List<TDynamicMethod> AdditionalMethods { get; set; } = new List<TDynamicMethod>();
+
+    public static async Task ParseArguments()
+    {
+        try
+        {
+            await Parse(Environment.GetCommandLineArgs().Skip(1).ToArray());
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex);
         }
     }
 
-    public class CMD : Attribute
+    public static void Add(string name, Delegate action)
     {
-        public string Alt { get; set; }
+        AdditionalMethods.Add(new TDynamicMethod(name, action));
+    }
 
-        public CMD(string Alt = "")
+    public static void Help()
+    {
+        Assembly? assembly = Assembly.GetEntryAssembly();
+        AssemblyName? assemblyName = assembly?.GetName();
+
+        if (assembly != null && assemblyName != null)
         {
-            this.Alt = Alt;
+            Console.WriteLine("****************************");
+            Console.WriteLine($"Assembly Name: {assemblyName.Name}");
+            var assemblyCompanyAttribute = assembly.GetCustomAttributes(typeof(AssemblyCompanyAttribute), false);
+            if (assemblyCompanyAttribute.Length > 0)
+            {
+                var companyAttribute = (AssemblyCompanyAttribute)assemblyCompanyAttribute[0];
+                Console.WriteLine($"Author: {companyAttribute.Company}");
+            }
+            Console.WriteLine($"Version: {assemblyName.Version}");
         }
+        Console.WriteLine("*********** HELP ***********");
+        Console.WriteLine();
+
+        var methods = GetMethods();
+        if (methods.Count > 0)
+        {
+            Console.WriteLine("Available commands:");
+            Console.WriteLine();
+
+            foreach (var m in methods)
+            {
+                Console.WriteLine($" {m.Name}:");
+                if (m.Parameters?.Length > 0)
+                {
+                    Console.WriteLine("  Parameters:");
+                    foreach (var p in m.Parameters)
+                    {
+                        Console.WriteLine($"    -{p.Name} ({p.Type.Name}){(p.HasDefaultValue ? $" (default: {p.DefaultValue})" : "")}");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("  This command does not take any parameters.");
+                }
+                Console.WriteLine();
+            }
+        }
+        else
+        {
+            Console.WriteLine("No commands are available.");
+        }
+    }
+
+    private static List<TDynamicMethod> GetMethods()
+    {
+        var methodsDefined = Assembly.GetEntryAssembly()?
+            .GetTypes()
+            .SelectMany(t => t.GetMethods())
+            .Where(m => m.GetCustomAttributes(typeof(CMD), false).Length > 0)
+            .ToList() ?? new List<MethodInfo>();
+        var methods = methodsDefined.Select(x => {
+            var param = x.GetParameters().Select(y => y.ParameterType).ToArray();
+            var dt = GenericActionTypeParameters(param.Length).MakeGenericType(param);
+            Delegate d = x.CreateDelegate(dt); 
+            return new TDynamicMethod(x.Name, d);
+        }).ToList();
+        methods.AddRange(AdditionalMethods);
+        return methods;
+    }
+
+    internal static void Error(string msg)
+    {
+        ConsoleColor prev = Console.ForegroundColor;
+        Console.ForegroundColor = ConsoleColor.DarkRed;
+        Console.Error.WriteLine(msg);
+        Console.ForegroundColor = prev;
+    }
+
+    private static Type GenericActionTypeParameters(int c)
+    {
+        // this is required due to a bug in .NET
+        return c switch
+        {
+            0 => typeof(Action),
+            1 => typeof(Action<>),
+            2 => typeof(Action<,>),
+            3 => typeof(Action<,,>),
+            4 => typeof(Action<,,,>),
+            5 => typeof(Action<,,,,>),
+            6 => typeof(Action<,,,,,>),
+            7 => typeof(Action<,,,,,,>),
+            8 => typeof(Action<,,,,,,,>),
+            9 => typeof(Action<,,,,,,,,>),
+            10 => typeof(Action<,,,,,,,,>),
+            11 => typeof(Action<,,,,,,,,,>),
+            12 => typeof(Action<,,,,,,,,,,>),
+            13 => typeof(Action<,,,,,,,,,,,>),
+            14 => typeof(Action<,,,,,,,,,,,,>),
+            15 => typeof(Action<,,,,,,,,,,,,,>),
+            16 => typeof(Action<,,,,,,,,,,,,,,>),
+            17 => typeof(Action<,,,,,,,,,,,,,,,>),
+            _ => typeof(Action),
+        };
+    }
+    
+    public static async Task Parse(params string[] args)
+    {
+        if (args.Length == 0 && DefaultToHelp) {
+            Help();
+            return;
+        } else if(args.Length == 0 && !DefaultToHelp)
+        {
+            Command.Error("No parameters presented");
+            return;
+        }
+        
+        TDynamicMethod? func = GetMethods().FirstOrDefault(x => x.Name.ToLower() == args[0].ToLower()) ?? null;
+        if (func == null)
+        {
+            Command.Error($"Command {args[0]} not found.");
+            return;
+        }
+
+        TArgument[]? newArgs = GetArguments(StringifyArguments(args.Skip(1)));
+        Task? invocation = func.Invoke(newArgs);
+        if (invocation != null)
+            await invocation;
+    }
+
+    private static string StringifyArguments(IEnumerable<string> args)
+    {
+        var sb = new StringBuilder();
+        foreach (var arg in args)
+        {
+            if(arg.Contains(' '))
+            {
+                sb.Append($"\"{arg}\"");
+            } else
+            {
+                sb.Append(arg);
+            }
+            sb.Append(' ');
+        }
+        return sb.ToString().Trim();
+    }
+
+    private static TArgument[] GetArguments(string input)
+    {
+        string pattern = "(?<name>-\\w+)(?:\\s+(?!-)(?<value>(?:\"[^\\\"]*\"|\\S+)))?";
+        var arguments = Regex.Matches(input, pattern)
+            .Cast<Match>()
+            .Select(m => new TArgument(m.Groups["name"].Value[1..], m.Groups["value"].Success ? m.Groups["value"].Value.Trim('"') : (object)true)).ToArray();
+        return arguments;
+    }
+}
+
+[AttributeUsage(AttributeTargets.Method)]
+public class CMD : Attribute
+{
+    public string Alt { get; set; }
+
+    public CMD(string Alt = "")
+    {
+        this.Alt = Alt;
     }
 }
